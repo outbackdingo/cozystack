a4025a84120051b4cce1e17e5ce047ecb5c06ee8
nbykov0 <166552198+nbykov0@users.noreply.github.com>
2025-05-20 20:38:48 +0300

    core/installer: multi-arch talos support
    Signed-off-by: nbykov0 <166552198+nbykov0@users.noreply.github.com>

--- hack/upload-assets.sh
+++ hack/upload-assets.sh
@@ -4,8 +4,18 @@ set -xe
 version=${VERSION:-$(git describe --tags)}
 
 gh release upload --clobber $version _out/assets/cozystack-installer.yaml
+
 gh release upload --clobber $version _out/assets/metal-amd64.iso
+gh release upload --clobber $version _out/assets/metal-arm64.iso
+
 gh release upload --clobber $version _out/assets/metal-amd64.raw.xz
+gh release upload --clobber $version _out/assets/metal-arm64.raw.xz
+
 gh release upload --clobber $version _out/assets/nocloud-amd64.raw.xz
+gh release upload --clobber $version _out/assets/nocloud-arm64.raw.xz
+
 gh release upload --clobber $version _out/assets/kernel-amd64
+gh release upload --clobber $version _out/assets/kernel-arm64
+
 gh release upload --clobber $version _out/assets/initramfs-metal-amd64.xz
+gh release upload --clobber $version _out/assets/initramfs-metal-arm64.xz
